7be955d2199570d86932e57fa6c922c4161c9371
aBurnley <aburnleylvx@outlook.com>
2020-07-08 11:34:37 -0400

    my first commit

---
+++ labs/TextHello/TextHello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHello
+{
+    public class Program
+    {//static means only 1 instance of the method can be used
+       public  static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            string name; //declares variable of type string named 'name'
+            Console.Write("Enter your name: "); //promtps user to enter name
+            name = Console.ReadLine();//ReadLine method reads input from the console up to a new line
+            Console.WriteLine($"Hello, {name}");//difference between Write and WriteLine is the latter writes to a new line
+        }
+    }
+}
